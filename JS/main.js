bc935a5aa4383e32e4879853e389fdc399c57aeb
Nao Aoyama <ea80002@gmail.com>
2023-12-05 17:32:49 +0900

    css has been changed

--- JS/main.js
+++ JS/main.js
@@ -4,13 +4,25 @@
 // document.getElementById('btn').addEventListener('click', function(){
   // });
   const navList = document.getElementsByClassName('nav--list__wrapper');
-  document.getElementById('btn').addEventListener('mouseover',()=>{
+  document.getElementById('btn').addEventListener('click',()=>{
     // console.log(navList);
     // navList.classList.toggle('active')
 
     gsap.to('.nav--list__wrapper',{
-      x:-300,
-      // duration:2
+      x:-1200,
+      duration:2
     })
 
-});
+    
+  });
+
+  // gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.to('#title',{
+    scrollTrigger:{
+      trigger:"#title",
+      toggleActions:"restart pause reverse pause"
+    },
+    rotation:360,
+    duration:3
+  });
